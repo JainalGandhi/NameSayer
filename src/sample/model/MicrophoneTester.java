6c102208e97a8de12c5e34e0d7a94704af9b431a
JainalGandhi <jainalgandhi@gmail.com>
2018-10-04 00:26:38 +1300

    Peak volume swapped to mean in micleveltester

--- src/sample/model/MicrophoneTester.java
+++ src/sample/model/MicrophoneTester.java
@@ -13,8 +13,8 @@ public class MicrophoneTester {
 	 * Return the maximum volume detected by the microphone to detect if its functioning
 	 */
     public double determineFunctioning() throws IOException, InterruptedException {
-        //Records audio and determines peak volume in the audio file
-        String TEST_ADUIO_VOLUME_COMMAND ="ffmpeg -y -f alsa -i default -t 0.005 ./names/test.wav 2> /dev/null; ffmpeg -i ./names/test.wav -af \"volumedetect\" -f null /dev/null 2>&1 | grep max_volume | awk -F': ' '{print $2}' | cut -d' ' -f1";
+        //Records audio and determines mean volume in the audio file
+        String TEST_ADUIO_VOLUME_COMMAND ="ffmpeg -y -f alsa -i default -t 0.005 ./names/test.wav 2> /dev/null; ffmpeg -i ./names/test.wav -af \"volumedetect\" -f null /dev/null 2>&1 | grep mean_volume | awk -F': ' '{print $2}' | cut -d' ' -f1";
         Process process = new ProcessBuilder("/bin/bash", "-c", TEST_ADUIO_VOLUME_COMMAND).start();
         process.waitFor();
 
