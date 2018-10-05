4e19b9f1628155bacb479ce007f7dd55e01571d0
JainalGandhi <jainalgandhi@gmail.com>
2018-10-05 23:11:40 +1300

    Misc. changes

--- src/sample/gui/PracticeNameController.java
+++ src/sample/gui/PracticeNameController.java
@@ -5,18 +5,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import sample.model.MicrophoneTester;
 import sample.model.Player;
 import sample.model.Score;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -44,10 +40,6 @@ public class PracticeNameController implements Initializable {
     private Score score = Score.getInstance();
     private double volume;
     private SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
-    private String latestRecordedName;
-    private MediaPlayer mediaPlayer;
-    private File currentDatabaseWavFile;
-    private int compareCount;
 
 
     @Override
@@ -111,10 +103,10 @@ public class PracticeNameController implements Initializable {
         this.recordButton.setText("Recording...");
         configureRecordingState(true);
         
-        this.latestRecordedName = "se206_" + formatter.format(new Date()) + "_" + this.player.getCurrentPlaylistName().replaceAll(" ", "_");
+        String latestRecordedName = "se206_" + formatter.format(new Date()) + "_" + this.player.getCurrentPlaylistName().replaceAll(" ", "_");
         Runnable task = new Thread( ()-> {
             try {
-                recordAttempt();
+                this.player.recordAttempt(latestRecordedName);
                 Platform.runLater( ()-> {
                 	// Enable play, save and compare after recording is made
                     configureRecordingState(false);
@@ -132,83 +124,34 @@ public class PracticeNameController implements Initializable {
         new Thread(task).start();
     }
     
-    public void recordAttempt() throws InterruptedException, IOException {
-        String RECORD_NAME_ATTEMPT_COMMAND = "rm names/temp/temp.wav;"
-        		+ " ffmpeg -f alsa -i default -t 5 -acodec pcm_s16le -ar 22050 -ac 1 ./names/temp/temp.wav 2> /dev/null; " +
-                "ffmpeg -hide_banner -i ./names/temp/temp.wav -af silenceremove=0:0:0:-1:2:-45dB ./names/temp/" + this.latestRecordedName + ".wav 2> /dev/null";
-        Process process = new ProcessBuilder("/bin/bash", "-c", RECORD_NAME_ATTEMPT_COMMAND).start();
-        process.waitFor();
-    }
+
 
     public void playUserButtonPressed() {
-    	this.player.stopAudioPlayback();
-    	String path = System.getProperty("user.dir") + "/names/temp/" + this.latestRecordedName + ".wav";
-    	this.mediaPlayer = new MediaPlayer(new Media(new File(path).toURI().toString()));
-    	this.mediaPlayer.play();
+        this.player.stopAudioPlayback();
+    	this.player.playLatestUserRecording();
     }
 
     public void saveUserButtonPressed() throws IOException {
     	// Remove any old user recordings for the same name
-    	String searchUser = "ls names/user | grep -i [[:digit:]]_" + this.player.getCurrentPlaylistName().replaceAll(" ", "_") + ".wav";
-		Process process = new ProcessBuilder("/bin/bash", "-c", searchUser).start();
-		InputStream stdout = process.getInputStream();
-		BufferedReader stdoutBuffered = new BufferedReader(new InputStreamReader(stdout));
-		String creation;
-		while ((creation = stdoutBuffered.readLine()) != null) {
-			String delete = "rm names/user/" + creation;
-	    	ProcessBuilder process2 = new ProcessBuilder("/bin/bash", "-c", delete);
-	    	process2.start();
-		}
-    	
-    	// Save current recording in names/user
-    	String saveCommand = "cp ./names/temp/" + this.latestRecordedName + ".wav ./names/user";
-    	ProcessBuilder process3 = new ProcessBuilder("/bin/bash", "-c", saveCommand);
-		process3.start();
-		
-		// Alert user to successful save
-		alert.redordingSaved();
+    	this.player.saveAttempt();
+        // Alert user to successful save
+        alert.redordingSaved();
     }
 
     public void doneButtonPressed() {
+        this.player.stopAudioPlayback();
         this.functioning = false;
         ((Stage) titleLabel.getScene().getWindow()).close();
     }
 
     public void compareButtonPressed() {
-    	this.currentDatabaseWavFile = this.player.getCurrentWav();
-    	this.player.stopAudioPlayback();
-    	int compare = compareSpinner.getValue();
-    	this.compareCount = compare;
+        this.player.stopAudioPlayback();
     	
     	// Recursively compare
-    	compareRecordings();
+    	this.player.compareRecordings((int) this.compareSpinner.getValue());
     }
     
-    public void compareRecordings() {
-    	if (this.compareCount == 0) {
-    		return;
-    	}
-    	
-    	this.compareCount--;
-    	this.mediaPlayer = new MediaPlayer(new Media(this.currentDatabaseWavFile.toURI().toString()));
-		this.mediaPlayer.play();
-		
-		this.mediaPlayer.setOnEndOfMedia(new Runnable() {
-			@Override
-			public void run() {
-				String path = System.getProperty("user.dir") + "/names/temp/" + latestRecordedName + ".wav";
-		    	mediaPlayer = new MediaPlayer(new Media(new File(path).toURI().toString()));
-	    		mediaPlayer.play();
-	    		
-	    		mediaPlayer.setOnEndOfMedia(new Runnable() {
-	    			@Override
-	    			public void run() {
-	    				compareRecordings();
-	    			}
-	    		});
-			}
-		});
-    }
+
 
     public void setColor(String color) {
         this.color = color;
