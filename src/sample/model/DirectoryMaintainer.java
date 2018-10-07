3b36e783e362b7d7d0d67d42caf872667f597054
JainalGandhi <jainalgandhi@gmail.com>
2018-10-07 17:02:13 +1300

    Remove println statement

--- src/sample/model/DirectoryMaintainer.java
+++ src/sample/model/DirectoryMaintainer.java
@@ -43,7 +43,6 @@ public class DirectoryMaintainer {
     public void writeBadQuality(String path) {
         Runnable task = new Thread( ()-> {
             String ADD_RATING_TO_DOCUMENT = "if [[ $(grep \"" + path + "\" ./names/badQualityRecordings.txt | wc -l) -eq 0 ]]; then echo \"" + path + "\" >> ./names/badQualityRecordings.txt; fi";
-            System.out.println(ADD_RATING_TO_DOCUMENT);
             try {
                 ProcessBuilder process = new ProcessBuilder("/bin/bash", "-c", ADD_RATING_TO_DOCUMENT);
                 process.start();
