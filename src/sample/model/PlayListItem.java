7b1529985466decc049df08fbf7f7ceb4d900f9e
JainalGandhi <jainalgandhi@gmail.com>
2018-10-19 00:13:54 +1300

    Code Commented

--- src/sample/model/PlayListItem.java
+++ src/sample/model/PlayListItem.java
@@ -11,31 +11,59 @@ public class PlayListItem {
 	private List<String> warnings = new ArrayList<>();
 	private int names;
 	public Boolean disablePractice = false;
-	
+
+	/**
+	 * Constructs the playlist item for a given name
+	 * @param name name of the item
+	 */
 	public PlayListItem(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * Adds warning to an item
+	 * @param warning item warning
+	 */
 	public void addWarning(String warning) {
 		warnings.add(warning);
 	}
-	
+
+	/**
+	 * Sets the wav file associated with the item
+	 * @param wav file associated with the item
+	 */
 	public void setWav(File wav) {
 		this.wav = wav;
 	}
-	
+
+	/**
+	 * Returns the wav file
+	 * @return wav file
+	 */
 	public File getWav() {
 		return this.wav;
 	}
-	
+
+	/**
+	 * Sets the number of names that are associated with the item
+	 * @param names number of names
+	 */
 	public void setNamesAmount(int names) {
 		this.names = names;
 	}
 
+	/**
+	 * Returns the name of the playlistitem
+	 * @return name
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Creates the now playing text for the current playlist item. Takes the name of the item and includes any labels
+	 * @return now playing label text
+	 */
 	public String getNowPlayingText() {
 		String text = "Now Playing: ";
 		if (warnings.size() >= names) {
@@ -52,7 +80,11 @@ public class PlayListItem {
 		}
 		return text;
 	}
-	
+
+	/**
+	 * Returns the warnings associated with the current playlist item
+	 * @return warnings for item
+	 */
 	private String getWarnings() {
 		StringBuilder warning = new StringBuilder("     Warning: Could not find ");
 		int iterator = 0;
